914bb1137569540ec2557f7681bba23daa00f41a
Daniel Rodriguez <kungary04@gmail.com>
2023-05-19 18:56:17 -0500

    design flat

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
-import Projects from './components/Projects/Projects';
-import Testimonials from './components/Testimonials/Testimonials';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+// import Projects from './components/Projects/Projects';
+// import Testimonials from './components/Testimonials/Testimonials';
+// import Contact from './components/Contact/Contact';
+// import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
